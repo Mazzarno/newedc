8affd0c53a9e45f981c6be11ae9579fe5c79826a
Mazzarno <alexis-germain@live.fr>
2023-10-16 12:52:37 +0200

    0.2.0

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "nuxt-swiper"],
   vite: { ssr: { noExternal: ["moment"] } },
+  nitro: { preset: "node" },
   swiper: {
     prefix: "Swiper",
     styleLang: "css",
